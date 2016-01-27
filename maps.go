da271832f108182dcbe091086ea470bdc7f7680e
Alan Zhao <alanzhao1@users.noreply.github.com>
2016-01-26 23:49:17 -0500

    Update maps.go in for loop
    Use of [:] is unnecessary as strings.Fields returns a slice and [:] doesn't copy the slice anyways like in Python.

--- maps.go
+++ maps.go
@@ -7,7 +7,7 @@ import (
 
 func WordCount(s string) (wordCount map[string]int) {
     wordCount = make(map[string]int)
-    for _, v := range strings.Fields(s)[:] {
+    for _, v := range strings.Fields(s) {
         wordCount[v]++
     }
     return wordCount
